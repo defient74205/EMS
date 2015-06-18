bb1ae8bcaea1cebc3b898877a8cc3cdaba747d4b
defient74205 <natsuki.fujimori@avasys.jp>
2015-06-18 00:23:09 +0000

    add colomun; promotor:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617082907) do
+ActiveRecord::Schema.define(version: 20150618001156) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150617082907) do
     t.text     "free"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "promotor"
   end
 
   create_table "joins", force: true do |t|
